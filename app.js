7390e8aaddd58c80408d3cab879aae24d6b8ba48
Swati shankar <swati.shankar22@gmail.com>
2017-05-31 00:30:54 +0530

    Javascript session2

---
+++ app.js
@@ -0,0 +1,14 @@
+var info ={
+	Name : 'Swati',
+	Age  : 24,
+	"Date Of Birth" : "15-1-1990",
+	"Place Of Birth" : "Blore"
+};
+
+console.log("Name : " + info.Name);
+
+console.log("Age : " + info['Age']);
+
+console.log("Date Of Birth : " + info["Date Of Birth"]);
+
+console.log("Place Of Birth : " + info["Place Of Birth"]);
